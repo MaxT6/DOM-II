ef6187d442a6cd48e97153abba60c00e0eaa0dc4
Maximilian Thiel <maximilianthiel@macbook-pro.myfiosgateway.com>
2022-03-08 17:19:48 -0500

    added wheel, keyup, keydown, resize, and scroll

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const colorButton = document.querySelectorAll(".btn");
 console.log(colorButton)
 
 colorButton.forEach(element => element.addEventListener("click", function() {
-  element.style.backgroundColor = "red";
+  element.style.backgroundColor = "green";
 }));
 
 // colorButton.addEventListener("click", function() {
@@ -59,8 +59,91 @@ const header = document.querySelector("header");
 window.addEventListener('copy', () => {
     navigator.clipboard.readText()
     .then(text => {
-        header.textContent += text
-    })
+        alert (`Hey! You do not have permisson to copy ${text}!`)
 })
+});
+
+// resize
+window.addEventListener('resize', () => {
+    alert ("Hey man don't resize me, thats not cool!");
+});
 
 console.log('test');
+
+
+
+//
+
+//scroll//
+const bodyColr = document.querySelector('body');
+window.addEventListener('scroll', () => {
+    bodyColr.style.backgroundColor = "gray";
+    setTimeout(function() {
+        bodyColr.style.backgroundColor='';
+    }, 500);
+},false);
+
+// window.addEventListener('keydown', evt => {
+//     if(evt.code == 'KeyA') {
+//         alert ('A is for Apple')
+//     } else if (evt.code == 'KeyB') {
+//         alert('B is for Bumblebee')
+//     }
+// })
+
+// keydown
+// window.addEventListener('keydown', evt => {
+//     if(evt.code == 'KeyA') {
+//         bodyColr.style.backgroundColor = "blue"
+//     } else if (evt.code == 'KeyB') {
+//         alert('B is for Bumblebee')
+//     }
+// })
+
+
+//keydown
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  bodyColr.addEventListener("keydown", evt => {
+      if(evt.code == 'KeyA') {
+        setBg();
+      } else if (evt.code == 'KeyB') {
+        bodyColr.style.backgroundColor = ""
+      }
+  });
+  
+
+  //keyup
+  window.addEventListener('keyup', () => {
+    setBg();
+})
+
+
+//wheel
+  function zoom(event) {
+    event.preventDefault();
+  
+    if (event.deltaY < 0) {
+      // Zoom in
+      scale *= event.deltaY * -2;
+    }
+    else {
+      // Zoom out
+      scale /= event.deltaY * 2;
+    }
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('div');
+  document.onwheel = zoom;
+
